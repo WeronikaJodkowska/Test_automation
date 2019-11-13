741ad2dcf4bb9f97323d8f9e1988b589e0ff6504
WeronikaJodkowska <31646812+WeronikaJodkowska@users.noreply.github.com>
2019-11-13 21:57:24 +0300

    Initial commit

---
+++ Test_automation/Automation.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_automation
+{
+    class Automation
+    {
+        IWebDriver SeleniumDriver;
+
+        [SetUp]
+        public void Start()
+        {
+            SeleniumDriver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test()
+        {
+            SeleniumDriver.Url = "https://www.google.by";
+            string text = "iTechArt";
+
+            IWebElement element1 = SeleniumDriver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input"));
+            element1.SendKeys(text);
+            element1.Submit();
+            Console.WriteLine("Page title is: " + SeleniumDriver.Title);
+            IWebElement element2 = SeleniumDriver.FindElement(By.XPath("//*[@id='rso']/div[1]/div/div/div/div/div[1]/a/h3"));
+            IWebElement element3 = SeleniumDriver.FindElement(By.XPath("//*[@id='rso']/div[1]/div/div/div/div/div[2]/div/span"));
+            Console.WriteLine(element2.Text.Contains(text));
+            Console.WriteLine(element3.Text.Contains(text));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            SeleniumDriver.Close();
+        }
+    }
+}
